27a55d83a813ae81e5e3c600033a022e3b05dbad
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2025-11-10 12:11:37 +0530

    Added DashboardInteractor.swift File

---
+++ Test_VIPER_Architecture_Project/Dashboard/DashboardInteractor.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardInteractor.swift
+//  Test_VIPER_Architecture_Project
+//
+//  Created by Sai Krishna on 11/10/25.
+//
+
+import Foundation
+
+protocol DashboardInteractorProtocol {
+    func fetchImages() async throws -> [RandomImage]
+}
+
+class DashboardInteractor: DashboardInteractorProtocol {
+    private let networkHandler: NetworkHandlerProtocol
+    
+    init(networkHandler: NetworkHandlerProtocol) {
+        self.networkHandler = networkHandler
+    }
+    
+    func fetchImages() async throws -> [RandomImage] {
+        try await networkHandler.fetchRandomImages()
+    }
+}
